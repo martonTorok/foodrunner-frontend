2eb239c271fd721adb0325f6b23aadaa2dffd8d4
tormartonz <tormartonz@gmail.com>
2019-02-06 16:12:45 +0100

    added basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuitemsComponent } from './pages/menuitems/menuitems.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'items', component: MenuitemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
