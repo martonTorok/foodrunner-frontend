6745cf222bdca540e4f5c0296a26f55ac1ec8fc0
tormartonz <tormartonz@gmail.com>
2019-02-10 20:02:59 +0100

    added password confirmation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -12,15 +12,17 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class RegisterComponent implements OnInit {
   registrationForm: FormGroup;
   email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required, Validators.minLength(6)])
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  confirmPassword = new FormControl('', [Validators.required, Validators.minLength(6)]);
 
   constructor(private authService: AuthService, private router: Router, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.registrationForm = new FormGroup({
       email: this.email,
-      password: this.password
-    });
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    }, { validators: PasswordValidation.MatchPassword });
   }
 
   onSubmit() {
@@ -49,9 +51,27 @@ export class RegisterComponent implements OnInit {
 
   getPasswordErrorMessage() {
     return this.password.hasError('required') ? 'Password is required.' :
-      this.password.hasError('minlength') ? 'Password must be minimum 6 character long.' : '';
+      this.password.hasError('minlength') ? 'Password must be minimum 6 characters long.' : '';
   }
 
+  getConfirmPasswordErrorMessage() {
+    return this.confirmPassword.hasError('required') ? 'Password is required.' :
+      this.confirmPassword.hasError('minlength') ? 'Password must be minimum 6 characters long.' : '';
+  }
+
+}
+
+export class PasswordValidation {
+
+  static MatchPassword(abstractControl: AbstractControl) {
+    let password = abstractControl.get('password').value;
+    let confirmPassword = abstractControl.get('confirmPassword').value;
+    if (password != confirmPassword) {
+      abstractControl.get('confirmPassword').setErrors({ MatchPassword: true })
+    } else {
+      return null
+    }
+  }
 }
 
 @Component({
